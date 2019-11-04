bd438ee95474f4cb3680bd2b9c5e7f2b1f74535b
vishalpatil0 <vishalpgatil10@gmail.com>
2019-11-04 14:11:47 +0530

    add comment to the fibonacci series

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -1,3 +1,4 @@
+//The Fibonacci sequence is a set of numbers that starts with a one or a zero, followed by a one, and proceeds based on the rule that each number (called a Fibonacci number) is equal to the sum of the preceding two numbers.
 #include<iostream>
 using namespace std;
 int main()
