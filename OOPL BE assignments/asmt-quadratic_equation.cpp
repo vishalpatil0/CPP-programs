aa1100913db9bd13a8e8e7967f5de2f07a60fcab
vishalpatil0 <vishalpgatil10@gmail.com>
2019-11-06 21:23:44 +0530

    quadratic

--- OOPL BE assignments/asmt-quadratic_equation.cpp
+++ OOPL BE assignments/asmt-quadratic_equation.cpp
@@ -1,91 +1,153 @@
 #include<iostream>
+#include<stdlib.h>
 #include<math.h>
 using namespace std;
-class quadratic
+
+class poly
 {
-	
-	int a,b,c,x;
-	public:
-		quadratic()
-		{
-			a=0;
-		        b=0;
-		        c=0;
-		}
-		friend quadratic operator>>(istream &qin,quadratic q);
-		friend quadratic operator<<(ostream &qout,quadratic q);
-		quadratic operator+(quadratic q);
-		void evaluate();
-		void root();
+
+     float a,b,c;
+
+     public:
+
+     poly()
+     {
+
+          a=b=c=0;
+
+     }
+
+     poly(float a,float b,float c)
+     {
+
+          this->a=a;
+          this->b=b;
+          this->c=c;
+          cout<<"\n"<<a<<" x^2 + "<<b<<" x + "<<c;
+
+     }
+
+     //display
+     friend void operator << (ostream &a,poly &p)
+     {
+     
+          cout<<"\n"<<p.a<<" x^2 + "<<p.b<<" x + "<<p.c;
+     
+     }
+     
+     friend istream &operator >> (istream &input,poly &p)
+     {
+     
+          input>>p.a>>p.b>>p.c;
+          return input;
+     
+     }
+     
+     poly operator + (poly p)
+     {
+     
+          poly temp;
+          temp.a=a+p.a;
+          temp.b=b+p.b;
+          temp.c=c+p.c;
+          return temp;
+     
+     }
+     
+     void eval()
+     {
+     
+          int x;
+          cout<<"\n Enter the value of x:    ";
+          cin>>x;
+          float sol=(a*x*x)+(b*x)+c;
+          cout<<"\n Evaluation of polynomial is:   "<<sol;
+     
+     }
+     
+     void solution()
+     {
+     
+          float x1,x2;
+          x1=( - b + sqrt ( b * b - 4 * a * c ) ) / ( 2 * a );
+          x2=( - b - sqrt ( b * b - 4 * a * c ) ) / ( 2 * a );
+          cout<<"\n Root of the quadratic equation are: "<<x1<<"  "<<x2;
+     
+     }
+
 };
-quadratic operator>>(istream &qin,quadratic q1)
-{
-	cout<<"Enter the value for a , b and c"<<endl;
-	qin>>q1.a>>q1.b>>q1.c;
-	return q1;
-}
-quadratic operator<<(ostream &qout,quadratic q1)
-{
-	qout<<q1.a<<"x*x+"<<q1.b<<"x+"<<q1.c<<endl;
-	return q1;
-}
-quadratic quadratic::operator+(quadratic q)
-{
-	quadratic temp;
-	temp.a=a+q.a;
-	temp.b=b+q.b;
-	temp.c=c+q.c;
-	return temp;
-}
-void quadratic::evaluate()
-{
-	cout<<"Enter the value for x"<<endl;
-	cin>>x;
-	double res=(a*x*x)+(b*x)+c;
-	cout<<"Result="<<res<<endl;
-}
-void quadratic::root()
-{
-	double rs=(b*b)-(4*a*c);
-	if(rs>0)
-	{
-		cout<<"Root are real and different"<<endl;
-		double root1=(-b+sqrt(rs))/(2*a);
-		double root2=(-b-sqrt(rs))/(2*a);
-		cout<<"Root1="<<root1<<endl;
-		cout<<"Root2="<<root2<<endl;
-	}
-	else if(rs==0)
-	{
-		cout<<"roots are real and equal"<<endl;
-		double root1=-b/2*a;
-		cout<<"Root="<<root1<<endl;
-	}
-	else
-	{
-		cout<<"Roots are complex and different"<<endl;
-	       	double real = -b/2*a;
-        	double img = sqrt(-rs)/2*a;
-        	cout<<"Root1="<<real<<"+"<<img<<"i"<<endl;
-        	cout<<"Root2="<<real<<"-"<<img<<"i"<<endl;
-    	}
-}
+
 int main()
 {
-    quadratic qt1,qt2,qt3;
-    cout<<"Enter the values of first quadratic:";
-    cin>>qt1;
-    cout<<"Enter the values of second quadratic:";
-    cin>>qt2;
-	cout<<qt1;
-	cout<<qt2;
-    qt3 = qt1 + qt2;
-    cout<<"Resulting Quadratic="<<endl;
-    cout<<qt3;
+     
+     int ch;
+     poly p1,p2,p3;
+     cout<<"\n Polynomial Operation ";
 
-    qt3.evaluate();
-    cout<<"Roots of Resulting Quadratic"<<endl;
-    qt3.root();
+     while(1)
+     {
 
-    return 0;
+          cout<<"\n\n Menu";
+          cout<<"\n 1. Create polynomial";
+          cout<<"\n 2. Accept polynomial";
+          cout<<"\n 3. Display polynomial";
+          cout<<"\n 4. Add polynomial";
+          cout<<"\n 5. Evaluate polynomial";
+          cout<<"\n 6. Solutions of polynomial";
+          cout<<"\n 7. Exit";
+          cout<<"\n Enter Choice:  ";
+          cin>>ch;
+
+          switch(ch)
+          {
+
+               case 1:
+                    cout<<"\n Polynomial after initialization ";
+                    cout<<p1;
+                    cout<<p2;
+                    break;
+
+               case 2:
+                    cout<<"\n Enter first polynomial (a,b,c):    ";
+                    cin>>p1;
+                    cout<<"\n Enter second polynomial (a,b,c):    ";
+                    cin>>p2;
+                    break;
+
+               case 3:
+                    cout<<"\n First polynomial is ";
+                    cout<<p1;
+                    cout<<"\n Second polynomial is ";
+                    cout<<p2;
+                    break;
+
+               case 4:
+                    p3=p1+p2;
+                    cout<<"\n Addition of two polynomial is    ";                    
+                    cout<<p3;
+                    break;
+
+               case 5:
+                    cout<<"\n For first polynomial";
+                    p1.eval();
+                    cout<<"\n For second polynomial";
+                    p2.eval();
+                    break;
+
+               case 6:
+                    cout<<"\n For first polynomial";
+                    p1.solution();
+                    cout<<"\n For second polynomial";
+                    p2.solution();
+                    break;
+
+               case 7:
+                    exit(1);
+                    break;                    
+
+          }
+
+     }
+     return 0;
+
 }
